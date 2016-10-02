f6796ad9c5f2f4b48710878756cf6bf7f35ce03c
Tatyana Khokhlova <tyh94@mail.ru>
2016-10-02 19:42:12 +0700

    WPF

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Controls;
-using System.Drawing;
 using System.Text.RegularExpressions;
+using System.Globalization;
+using Microsoft.Win32;
 using Paint;
 
 namespace WPF
@@ -33,7 +34,7 @@ namespace WPF
 
         private void init()
         {
-            initWindow();
+            //initWindow();
             _chartManager = new ChartManager();
             List<ChartData> chartData;
             FileManager.InputFromFile("Resources/input.txt", out chartData);
@@ -52,6 +53,7 @@ namespace WPF
 
         private void SetupPanel()
         {
+            panel1.Children.Clear();
             for (int i = 0, k = 0; i < _chartManager.ChartDataList.Count; i++, k++)
             {
                 List<MyColor> colors = new List<MyColor>
@@ -63,7 +65,6 @@ namespace WPF
                 };
 
                 ChartData chartData = _chartManager.ChartDataList[i];
-                chartData.ChartColor = colors[i % colors.Count].color;
                 
                 Button buttonShowPoints = new Button();
                 buttonShowPoints.Width = 160;
@@ -76,7 +77,7 @@ namespace WPF
                 Canvas.SetTop(buttonShowPoints, 20 + (k * 40));
 
                 ComboBox comboBox = new ComboBox();
-                comboBox.Name = "ComboBox" + i + "";
+                comboBox.Name = "ButtonColor" + i + "";
                 comboBox.ItemsSource = colors;
                 comboBox.Width = 75;
                 comboBox.Height = 20;
@@ -155,15 +156,48 @@ namespace WPF
             //    }
             //    pictureBox1.Invalidate();
         }
-
-    private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //ComboBox combox = (ComboBox)sender;
-            //MyColor color = (MyColor)combox.SelectedItem;
-            //Button button = (Button)panel1.Controls[combox.Name];
-            //button.BackColor = color.color;
-            //_chartManager.ChartDataList[Convert.ToInt32(combox.Name)].ChartColor = color.color;
-            //pictureBox1.Invalidate();
+            ComboBox combox = (ComboBox)sender;
+            MyColor color = (MyColor)combox.SelectedItem;
+            Button button = (Button)findByName(combox.Name, panel1.Children);
+            button.Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(color.color.A, color.color.R, color.color.G, color.color.B));
+            string mynumber = Regex.Replace(button.Name, @"\D", "");
+            _chartManager.ChartDataList[Convert.ToInt32(mynumber)].ChartColor = color.color;
+            Invalidate();
+        }
+
+        private UIElement findByName(string name, UIElementCollection collection)
+        {
+            foreach(UIElement element in collection)
+            {
+                if (element is TextBox)
+                {
+                    TextBox txt = (TextBox)element;
+                    if (txt.Name == name)
+                    {
+                        return txt;
+                    }
+                }
+                else if (element is ListBox)
+                {
+                    ListBox lst = (ListBox)element;
+                    if (lst.Name == name)
+                    {
+                        return lst;
+                    }
+                }
+                else if (element is Button)
+                {
+                    Button btn = (Button)element;
+                    if (btn.Name == name)
+                    {
+                        return btn;
+                    }
+                }
+            }
+            return null;
         }
 
         //private void addLineToolStripMenuItem_Click(object sender, EventArgs e)
@@ -185,5 +219,124 @@ namespace WPF
         //        pictureBox1.Invalidate();
         //    }
         //}
+
+        #region Drawing
+
+        private void Invalidate()
+        {
+            pictureBox1.Children.Clear();
+            if (_chartManager.ChartDataList.Count != 0)
+            {
+                DrawChart();
+                DrawAxisX();
+                DrawAxisY();
+            }
+        }
+
+        private void DrawChart()
+        {
+            foreach (var chartData in _chartManager.ChartDataList)
+            {
+                var points = chartData.Points;
+                var width = pictureBox1.ActualWidth;
+                var height = pictureBox1.ActualHeight;
+                var margin = ChartMargin;
+                var minX = Math.Floor(_chartManager.MinX);
+                var maxX = Math.Ceiling(_chartManager.MaxX);
+                var minY = Math.Floor(_chartManager.MinY);
+                var maxY = Math.Ceiling(_chartManager.MaxY);
+                for (var i = 0; i < points.Count - 1; i++)
+                {
+                    var x1 = margin + (points[i].X - minX) / (maxX - minX) * (width - margin);
+                    var y1 = height - margin - (points[i].Y - minY) / (maxY - minY) * (height - margin);
+                    var x2 = margin + (points[i + 1].X - minX) / (maxX - minX) * (width - margin);
+                    var y2 = height - margin - (points[i + 1].Y - minY) / (maxY - minY) * (height - margin);
+                    var r = chartData.ChartColor.R;
+                    var g = chartData.ChartColor.G;
+                    var b = chartData.ChartColor.B;
+                    var color = new SolidColorBrush(System.Windows.Media.Color.FromRgb(r, g, b));
+                    DrawLine(x1, y1, x2, y2, color);
+                }
+            }
+        }
+
+        private void DrawAxisX()
+        {
+            var width = pictureBox1.ActualWidth;
+            var height = pictureBox1.ActualHeight;
+            var margin = ChartMargin;
+            var minX = Math.Floor(_chartManager.MinX);
+            var maxX = Math.Ceiling(_chartManager.MaxX);
+            var length = maxX - minX;
+            var stepX = (length / 10);
+            var count = length / stepX;
+            var stepW = (width - margin) / count;
+            DrawLine(margin, height - margin, width, height - margin, Brushes.Black);
+            var x = margin;
+            var y = height - margin;
+            for (var i = 0; x < width; i++)
+            {
+                DrawLine(x, y, x, y - 5, Brushes.Black);
+                var text = Math.Round(minX + stepX * i, 2).ToString(CultureInfo.InvariantCulture);
+                DrawText(x, y, text);
+                x += stepW;
+            }
+        }
+
+        private void DrawAxisY()
+        {
+            var height = pictureBox1.ActualHeight;
+            var margin = ChartMargin;
+            var minY = Math.Floor(_chartManager.MinY);
+            var maxY = Math.Ceiling(_chartManager.MaxY);
+            var length = maxY - minY;
+            var stepY = (length / 10);
+            var count = length / stepY;
+            var stepH = (height - margin) / count;
+            DrawLine(margin, 0, margin, height - margin, Brushes.Black);
+            var x = margin;
+            var y = height - margin;
+            for (var i = 0; y > 0; i++)
+            {
+                DrawLine(x, y, x + 5, y, Brushes.Black);
+                var text = Math.Round(minY + stepY * i, 2).ToString(CultureInfo.InvariantCulture);
+                DrawText(x, y, text, 90);
+                y -= stepH;
+            }
+        }
+
+
+        private void DrawText(double x, double y, string text, double angle = 0)
+        {
+            var textBlock = new TextBlock
+            {
+                Text = text,
+                RenderTransform = new RotateTransform(angle)
+            };
+            Canvas.SetLeft(textBlock, x);
+            Canvas.SetTop(textBlock, y);
+            pictureBox1.Children.Add(textBlock);
+        }
+
+        private void DrawLine(double x1, double y1, double x2, double y2, System.Windows.Media.Brush color)
+        {
+            var line = new Line
+            {
+                Stroke = color,
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2
+            };
+            pictureBox1.Children.Add(line);
+        }
+
+        #endregion Drawing
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Invalidate();
+        }
     }
 }
